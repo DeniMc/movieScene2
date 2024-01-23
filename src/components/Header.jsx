dfcb7ccca1a9a79c72f5167f789573eb346f5f6a
Denis McArdle <denimcardle@gmail.com>
2024-01-23 11:46:49 +0100

    Trending and Top Rating fetches added

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -15,7 +15,7 @@ function Header() {
       <div className="flex items-center gap-4">
         <DarkModeSwitch />
          <Link href={'/'} className="flex gap-1 items-center">
-         <span className='text-2xl font-bold bg-amber-500 py-1 px-2 rounded-lg'>IDMB</span>
+         <span className='text-2xl font-bold bg-amber-500 py-1 px-2 rounded-lg'>MovieScene</span>
          <span className='text-xl hidden sm:inline'>Clone</span>
        </Link>
       </div>
