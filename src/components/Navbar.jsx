e8c9a745a33167128ff81c4ad571b45fd2f5a9bf
Denis McArdle <denimcardle@gmail.com>
2025-05-22 00:54:55 +0100

    wrapped NavbarItems in Suspense

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
+import { Suspense } from 'react';
 import NavBarItem from './NavBarItem'
 
 export default function Navbar( {title, param} ) {
   return (
     <div className='flex bg-[var(--background)] text-[var(--primary-dark)] p-4 lg:text-lg justify-center gap-6'>
+        <Suspense fallback={null}>
         <NavBarItem title="Trending" param="fetchTrending"/>
         <NavBarItem title="Top Rated" param="fetchTopRated"/>
+         </Suspense>
     </div>
   )
 }
