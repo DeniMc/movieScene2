f06bc9ebc280186b3088343a411d9f63497ed79f
Denis McArdle <denimcardle@gmail.com>
2025-05-21 21:49:26 +0100

    4 colour then selector added still needs some work.

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,5 @@
 import { Inter } from "next/font/google";
-import "./globals.css";
+import '../styles/globals.css';
 import Header from '@/components/Header';
 import Navbar from '@/components/Navbar';
 import Providers from "./Providers";
